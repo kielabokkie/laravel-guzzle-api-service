a8a0d26d067ef7e618361328797fca365dac2771
Wouter Peschier <kielabokkie@gmail.com>
2019-10-25 17:18:05 +1300

    Apply fixes from StyleCI (#11)

--- config/api-service.php
+++ config/api-service.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    /**
+    /*
      * Enable logging of request and responses to storage/logs/api-service.log
      */
     'logging_enabled' => env('API_SERVICE_LOGGING_ENABLED', false),
